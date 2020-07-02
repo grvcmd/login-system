f1e781a854bd12b8f77fd2f18421a31d385e834b
Ken Valverde <verify>
2020-07-02 16:22:55 -0500

    Created class User

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+A simple login program
+
+Created by Ken Valverde
+
+"""
+
+
+class User:
+    def __init__(self, name, username, password):
+        self.name = name
+        self.username = username
+        self.password = password
+
+    # Functions will go here
+
+
+if __name__ == '__main__':
+    pass
